74ec1c9244d739cb9bfca402549467048d5fc02c
raydenhacker <raydenhacker@gmail.com>
2026-09-14 21:40:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ const CACHE_NAME = 'qr-pwa-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 // Cache semua resource saat pertama kali install
